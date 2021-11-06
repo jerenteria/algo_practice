ed6523d586035db804d109b8d11e55552ad44493
jerenteria <renteria.juan96@gmail.com>
2021-11-06 12:18:41 -0700

    more algos

--- practice.py
+++ practice.py
@@ -75,8 +75,55 @@ def reverse_integer(n):
     return reversed_integer
 
 if __name__ == '__main__':
-    print(reverse_integer(1234))
+    print(reverse_integer(12345678))
 
+## anagram(create a new word/phrase using same letters ex. restful => fluster)
+def is_anagram(str1, str2):
+    # if length of the sgtrings differ - they are nit anagrams
+    if len(str1) != len(str2):
+        return False
+    
+    # we have to sort the letters of the strings and then we have to compare
+    ## the letters with the same indexes
+    ## this is the bottlenect because it has 0(NlogN)
+    str1 = sorted(str1)
+    str2 = sorted(str2)
+    print(str1)
+    print(str2)
+
+    ## after that we have to check the letters with the same indexes
+    for i in range(len(str1)):
+        if str1[i] != str2[i]:
+            return False
+    
+    return True
+
+    ## overall running time is 0(NlogN) + 0(N) =(NlogN)
+
+if __name__ == '__main__':
+    s1 = ['f','l','u','s','t','e','r']
+    s2 = ['r','e','s','t','f','u','l',]
+    print(is_anagram(s1, s2))
+
+## find duplicate in linear time complexity without needing extra memory
+
+def find_duplicates(nums):
+    ## iterate through array
+    for num in nums:
+        ## use absolute value of given number which has to be greater than 0
+        if nums[abs(num)] >= 0:
+            ## set nums with absolute value num to be equal to -nums with absolute value num
+            nums[abs(num)] = -nums[abs(num)]
+        else:
+            print('Repition found: %s' % str(abs(num)))
+
+if __name__ == '__main__':
+
+    ## THIS METHOD CANNOT HANDLE VALUES SMALLER THAN 0!!!
+    ## maximum items has to be smaller than size of the list
+
+    n = [2,6,5,1,4,3,2,5]
+    find_duplicates(n)
 
 
 
