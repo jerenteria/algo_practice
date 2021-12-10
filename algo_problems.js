f6c81cba410eef3d42c638968db5ded5aa57504d
jerenteria <renteria.juan96@gmail.com>
2021-12-09 19:33:32 -0800

    more javascript algo practice

---
+++ algo_problems.js
@@ -0,0 +1,17 @@
+// setting and swapping
+// set myNumber to 42. Set myName to your name. Now swap myNumber into myName and vice versa
+
+
+
+// Print integers from -52 to 1066 using a FOR loop.
+for(i = -52; i <= 1066; i++) {
+    console.log(i)
+}
+
+
+// Create beCheerful(). Within it,
+//console.log string "good morning!" Call it 98 times.
+
+for(let goodMorning = 0; goodMorning <= 98; goodMorning++) {
+    console.log("Good morning!")
+}
--- javascript_practice.js
+++ javascript_practice.js
@@ -13,3 +13,28 @@ for(var num = 10; num > 2; num = num - 1)
 {
     console.log('num is currently', num);
 }
+
+console.log("------------------------")
+
+// code stops when reaching number 3
+var num = 1;
+while(num < 5) 
+{
+    if(num == 3) // causes code to break when reaching number 3
+    {
+        break; // exits the loop and enters the following loop
+    }
+    console.log("Im counting! The number is ", num);
+    num = num + 1;
+}
+
+console.log("-------------------------------------")
+
+// skips the number 3
+for(var nums = 1; nums < 5; nums += 1) {
+    if(nums == 3) {
+        continue;
+    }
+    console.log("Im counting! The number is ", nums);
+}
+
