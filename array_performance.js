a83f050a8931315fb3a030d902fc3d14d0e4080b
jerenteria <renteria.juan96@gmail.com>
2021-12-25 11:20:13 -0800

    more algos

--- array_performance.js
+++ array_performance.js
@@ -1,1 +1,9 @@
-// Welcome
+// Objects are unordered data structures
+// work better wehen you need fast access/ insertion and removal/ accessing data
+
+let instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3,4]
+}
+object.keys()
