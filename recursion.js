4578076f68cce4fefcd35397cde5934b425a16b5
jerenteria <renteria.juan96@gmail.com>
2022-01-14 19:55:22 -0800

    algos

--- recursion.js
+++ recursion.js
@@ -30,3 +30,29 @@ function factorial(num) {
     if(num === 1) return 1; // base case
     return num * factorial(num - 1)
 }
+
+// write a recursion function that returns odd values 
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+collectOddValues([1,2,3,4,5])
+
+// write a recursive function which takes in an array of numbers and returns the product of them all
+function productOfArray(str) {
+    if(arr.length === 0) {
+        return 1;
+    }
+    return arr[0] * productOfArray(arr.slice(1));
+}
