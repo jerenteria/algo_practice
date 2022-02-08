88071bc401992af19039348d21ae8a5f2f4804fe
jerenteria <renteria.juan96@gmail.com>
2022-02-07 21:38:11 -0800

    algos

--- merge_sort.js
+++ merge_sort.js
@@ -5,3 +5,4 @@
 // exploits the fact that arrays of 0 or 1 element are always sorted
 // works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
 
+//
