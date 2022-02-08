fd62a4128858c51f2501f2c6a221a291ddb09fd8
jerenteria <renteria.juan96@gmail.com>
2022-02-07 21:36:26 -0800

    algos

---
+++ merge_sort.js
@@ -0,0 +1,7 @@
+// faster sorts can improve time complexity from O(n^2) to O(n log n)
+
+
+// MERGE SORT: a combination of two things- merging and sorting
+// exploits the fact that arrays of 0 or 1 element are always sorted
+// works by decomposing an array into smaller arrays of 0 or 1 elements, then building up a newly sorted array
+
