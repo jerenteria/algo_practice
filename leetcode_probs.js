eb17acd60ae00feb13431c07cc334a97a6b9ca12
jerenteria <renteria.juan96@gmail.com>
2022-03-09 20:09:00 -0800

    leetcode

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -5,6 +5,33 @@ var containsDuplicate = function(nums) {
         if(nums[i] == nums[j]) {
             return true;
         }
-        return false;
+    }
+    return false;
+};
+containsDuplicate([1,2,3,4])
+
+// TWO SUM: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+var twoSum = function(nums, target) {
+    // set variable to empty array to keep track of previous values
+    previousValues = {}
+    // iterate through array
+    for(i = 0; i < nums.length; i++) {
+        // current number is index of i in nums array
+        const currentNumber = nums[i]
+        // variable neededValue is value we need which is target - currentNumber
+        const neededValue = target - currentNumber
+        // check if we have a key of neededValue at that index
+        const index2 = previousValues[neededValue]
+        // if index2 is not null then we have something for that equals that target
+        if(index2 != null) {
+            return [index2, i]
+        }
+        else {
+            // store the current number in i if we dont have value that = target
+            previousValues[currentNumber] = i
+        }
     }
 };
+Input: nums = [2,7,11,15], target = 9
