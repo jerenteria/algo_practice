e9354271e1527bf43c0d85056e0d82ef608ca254
jerenteria <renteria.juan96@gmail.com>
2022-04-03 11:40:38 -0700

    linked list question on leetcode

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -178,3 +178,34 @@ var fairCandySwap = function(aliceSizes, bobSizes) {
     }
 }
 
+
+// 21. Merge Two Sorted Lists:
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+// ex: 1 --> 2 --> 4
+    // 1 --> 3 --> 4
+// output: 1 --> 1--> 2 --> 3 --> 4 --> 4
+
+var mergeTwoLists = function(list1, list2) {
+    const dummy = new ListNode(-Infinity);
+    let prev = dummy;
+    
+    // while list 1 and list 2 are true check the values
+    while(list1 && list2) {
+        if(list1.val <= list2.val) {
+            prev.next = list1;
+            prev = list1;
+            list1 = list1.next
+        } else {
+            prev.next = list2;
+            prev = list2;
+            list2 = list2.next;
+        }
+    }
+    if(!list1) prev.next = list2;
+    if(!list2) prev.next = list1;
+    
+    return dummy.next;
+};
+mergeTwoLists([1,2,4],[1,3,4])
