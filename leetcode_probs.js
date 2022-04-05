82f23fd6da6b153a7eafd2a4c8cc803a5a87072f
jerenteria <renteria.juan96@gmail.com>
2022-04-04 21:19:06 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -209,3 +209,18 @@ var mergeTwoLists = function(list1, list2) {
     return dummy.next;
 };
 mergeTwoLists([1,2,4],[1,3,4])
+
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+var reverseList = function(head) {
+    let current = head;
+    let prev = null;
+    let next;
+
+    while(current !== null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next
+    }
+    return prev;
+};
