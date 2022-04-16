2185c14c317d8c7ef850d63a2db4070c71d9ab55
jerenteria <renteria.juan96@gmail.com>
2022-04-15 21:48:33 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -318,5 +318,5 @@ deleteNode([4,5,1,9], 5)
 // Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head
 
 var removeElements = function(head, val) {
-    
+    hash = {};
 };
