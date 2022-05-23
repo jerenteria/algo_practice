8051df9e6429a9808c4dfa032c13d192cf291bdc
jerenteria <renteria.juan96@gmail.com>
2022-05-22 21:12:24 -0700

    solved some algo problems on leetcode

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -380,18 +380,19 @@ var countNegatives = function(grid) {
 console.log(countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]));
 
 
-
+// Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+// Return any array that satisfies this condition.
 
 var sortArrayByParity = function(nums) {
     let even = [];
     let odd = [];
     for(i = 0; i < nums.length; i++) {
         if(nums[i] % 2 == 0) {
-            even.push(nums[i])
-            console.log(even)
-        }        
-        odd.push(nums[i])
+            even.push(nums[i]);
+        }     
+        else if(nums[i] % 2 !== 0) {
+            odd.push(nums[i]);
+        }
     }
     return even.concat(odd);
 };
-sortArrayByParity([3,1,2,4]);
