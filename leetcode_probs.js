4ecfc8a1ad78c5de8b596fe7c438d1f2f50d75f0
jerenteria <renteria.juan96@gmail.com>
2022-05-24 21:05:58 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -399,11 +399,19 @@ var sortArrayByParity = function(nums) {
 
 
 
+// You are given an array of integers nums. You are also given an integer original which is the first number that needs to be searched for in nums.
+// You then do the following steps:
+// If original is found in nums, multiply it by two (i.e., set original = 2 * original).
+// Otherwise, stop the process.
+// Repeat this process with the new number as long as you keep finding the number.
+// Return the final value of original.
+
 var findFinalValue = function(nums, original) {
-    for(i = 0; i < nums.length; i++) {
-        if(nums[i] === original) {
-            (nums[i] * 2) = original;
+    let target = original;
+    for(let i=0; i< nums.length;i++){
+        if(nums.includes(target)){
+            target*=2;
         }
     }
-    return original;
-}; 
+    return target;
+};
