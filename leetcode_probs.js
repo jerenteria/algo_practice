dffa6c732d8db5068d670c27526974c7c4e6e5d4
jerenteria <renteria.juan96@gmail.com>
2022-06-07 21:26:05 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -489,3 +489,9 @@ var getDecimalValue = function(head) {
     }
     return total;
 };
+
+
+// 922. Sort Array By Parity II
+// Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
+// Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
+// Return any answer array that satisfies this condition.
