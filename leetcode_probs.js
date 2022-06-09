6f5c8a59cca7a3b9346e6b6b41418be6097b56f0
jerenteria <renteria.juan96@gmail.com>
2022-06-08 20:33:12 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -495,3 +495,16 @@ var getDecimalValue = function(head) {
 // Given an array of integers nums, half of the integers in nums are odd, and the other half are even.
 // Sort the array so that whenever nums[i] is odd, i is odd, and whenever nums[i] is even, i is even.
 // Return any answer array that satisfies this condition.
+
+var sortArrayByParityII = function(nums) {
+    let map = [];
+    const second = (nums[i] % 2 != 0)
+    for(i = 0; i < nums.length / 2; i++) {
+        if(nums[i] % 2 == 0) {
+            map.push(nums[i]);
+            map.push(nums[second]);
+        }
+
+    }
+    return map;
+};
