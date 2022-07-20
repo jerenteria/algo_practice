ed42b44dda8d1db756df810a51a7470fd2b3afc5
jerenteria <renteria.juan96@gmail.com>
2022-07-19 18:40:40 -0700

    another day of algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -596,3 +596,24 @@ var countMatches = function(items, ruleKey, ruleValue) {
     return count;
 };
 
+// 2011. Final Value of Variable After Performing Operations
+// There is a programming language with only four operations and one variable X:
+
+// ++X and X++ increments the value of the variable X by 1.
+// --X and X-- decrements the value of the variable X by 1.
+// Initially, the value of X is 0.
+
+// Given an array of strings operations containing a list of operations, 
+// return the final value of X after performing all the operations.
+
+var finalValueAfterOperations = function(operations) {
+    let x = 0;
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i].includes("+")) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    return x;
+};
