20d6fa5fe5bf9c3e140cc4a3ff2c69e15adcba2b
jerenteria <renteria.juan96@gmail.com>
2022-07-20 19:55:47 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -617,3 +617,20 @@ var finalValueAfterOperations = function(operations) {
     }
     return x;
 };
+
+// 283. Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// Note that you must do this in-place without making a copy of the array.
+
+var moveZeroes = function(nums) {
+    for(i = 0; i < nums.length; i++) {
+        let j = nums[i] + 1;
+        if(nums[i] === 0) {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+        }
+    }
+    return nums;
+};
