88499113ed5522aeee386adf9c627a602b34c12d
jerenteria <renteria.juan96@gmail.com>
2022-08-31 18:32:59 -0700

    algos

---
+++ practice.js
@@ -0,0 +1,16 @@
+map = [];
+var value = function(nums, val) {
+    for(i = 0; i < nums.length; i++) {
+        if(nums[i] === val) {
+            map.push(i);
+        }
+    }
+    return map;
+
+
+nums.push(2);
+nums.push(3);
+nums.push(4)
+};
+console.log(value());
+
---
+++ practice1.js
@@ -0,0 +1,8 @@
+function print() {
+    var x = '5';
+    alert(x+y);
+}
+var x = 3;
+var y = 2;
+print()
+alert(x+y);
