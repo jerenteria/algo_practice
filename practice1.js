02c446d0ebb008e32fd7a11fdbeb0aae1cefc9ba
jerenteria <renteria.juan96@gmail.com>
2022-09-02 17:59:21 -0700

    just bombed interview questions

--- practice1.js
+++ practice1.js
@@ -1,8 +1,24 @@
-function print() {
-    var x = '5';
-    alert(x+y);
+// function print() {
+//     var x = '5';
+//     alert(x+y);
+// }
+// var x = 3;
+// var y = 2;
+// print()
+// alert(x+y);
+
+function isAlmostPalindrome(string1, string2) {
+    let map = [];
+    let counter = 0;
+    for(i = 0; i < string1.length; i++) {
+        for(j = 0; j < string2.length; i++) {
+            if(string1.length !== string2.length) {
+                return false;
+            }
+            if(string1 === string2) {
+                return true;
+            }
+        }
+    }
 }
-var x = 3;
-var y = 2;
-print()
-alert(x+y);
+isAlmostPalindrome('abccba', 'abccfb')
