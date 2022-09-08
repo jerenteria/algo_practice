a79dba0806d8f4790058c65577ee8ff63eabbda7
jerenteria <renteria.juan96@gmail.com>
2022-09-07 20:43:53 -0700

    preorder

--- binary_search_trees.js
+++ binary_search_trees.js
@@ -163,7 +163,17 @@ tree.insert(20)
                 constructor() {
                     this.root = null;
                 }
-
+                DFSPreorder() {
+                    var data = [];
+                    var current = this.root;
+                    function traverse(node) {
+                        data.push(node.value);
+                        if(node.left) traverse(node.left);
+                        if(node.right) traverse(node.right);
+                    }
+                    traverse(this.root);
+                    return data;
+                }
             }        
 
         // DFS: POSTORDER
