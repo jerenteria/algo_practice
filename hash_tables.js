6001d37f3bcdb822a223664f55fceca9a1ab5743
jerenteria <renteria.juan96@gmail.com>
2022-09-17 12:08:50 -0700

    just finished hash tables on to graphs

--- hash_tables.js
+++ hash_tables.js
@@ -1,6 +1,19 @@
 // WHAT IS A HASH TABLE(HASH MAP): USED TO STORE KEY-VALUE PAIRS; ARE LIKE ARRAYS BUT KEYS ARE NOT ORDERED
     // UNLIKE ARRAYS, HASHTABLES ARE FAST FOR ALL OF THE FOLLOWING OPERATIONS, FINDING VALUES, ADDING NEW VALUES, AND REMOVING VALUES
 
+// BIG O OF HASH TABLES:
+    // INSERT - O(1)
+    // DELETION - O(1)
+    // ACCESS - O(1)
+
+// RECAP: 
+    // HASH TABLES ARE COLLECTIONS OF KEY-VALUE PAIRS
+    // HASH TABLES CAN FIND VALUES QUICKLY GIVEN A KEY
+    // HASH TABLES CAN ADD NEW KEY VALUES QUICKLY
+    // HASH TABLES CAN STORE DATA IN A LARGE ARRAY, AND WORK BY HASHING THE KEYS
+    // A GOOD HASH SHOULD BE FAST DISTRIBUTE KEYS UNIFORMLY, AND BE DETERMINISTIC
+    // SEPERATE CHAINING AND LINEAR PROBING ARE TWO STRATEGIES USED TO DEAL WITH TWO KEYS THAT HASH TO THE SAME INDEX
+
 // PYTHON HAS DICTIONARIES
 // JAVASCRIPT HAS OBJECTS AND MAPS
 // JAVA, GO, & SCALA HAVE MAPS
@@ -72,8 +85,56 @@ class HashTable {
         // which becomes the child to the parent array that was already there
         this.keyMap[index].push([key, value]);
     }
-     
+    get(key) {
+        let index = this._hash(key);
+        if(this.keyMap[index]) {
+            // once we found correct spot loop through that specific index
+            for(let i = 0; i < this.keyMap[index].length; i++) {
+                // if the value at that index is the same as the key
+                if(this.keyMap[index][i][0] === key) {
+                    // return the value at that index
+                    return this.keyMap[index][i][1]
+                } 
+            }
+        }
+        return undefined;
+    }
+    value() {
+        let valuesArr = [];
+        for(let i = 0; i < this.keyMap.length; i++) {
+            if(this.keyMap[i]) {
+                // loop thorough all the values and check if there is any duplicates
+                for(let j = 0; j < this.keyMap[i].length; j++) {
+                    // keeps duplicates out
+                    if(!valuesArr.includes(this.keyMap[i][j][i]))
+                    // if no duplicates push
+                    valuesArr.push(this.keyMap[i][j])
+                }
+            }
+        }
+        return valuesArr;
+    }
+    keys() {
+        let valuesArr = [];
+        for(let i = 0; i < this.keyMap.length; i++) {
+            if(this.keyMap[i]) {
+                // loop thorough all the values and check if there is any duplicates
+                for(let j = 0; j < this.keyMap[i].length; j++) {
+                    // each key is stored at index 0(first val) instead of the the second(idx 1)
+                    if(!keysArr.includes(this.keyMap[i][j][0]))
+                    // if no duplicates push
+                    keysArr.push(this.keyMap[i][j])
+                }
+            }
+        }
+        return keysArr;
+    }
 }
 let ht = new HashTable();   
-ht.set("hellow world", "goodbye!!")
+ht.set("green", "#f0000d")
+ht.set("red", "#f79874")
+ht.set("black", "#f00000")
+ht.set("orange", "#f89345")
+ht.set("yellow", "#f23487")
+ht.set("purple", "#f487529")
 
