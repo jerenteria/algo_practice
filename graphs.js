b1f2ba51717a66aecc86d58e6d8bece06c688f07
jerenteria <renteria.juan96@gmail.com>
2022-09-18 19:46:35 -0700

    learning about graphs

---
+++ graphs.js
@@ -0,0 +1,85 @@
+// what is a graph?
+    // collection of nodes and connections between those nodes
+
+// uses for graphs
+    // social networking
+        // connections between friends on a social site(facebook/instagram)
+    // location/mapping
+        // shortest path on a map(google maps)
+    // routing algorithms
+    // visual hierarchy
+    // file system optimizations
+    // EVERYWHERE
+
+// types of graphs
+    // vertex - a node
+    // edge - connection between nodes
+
+    // undirected graph = no direction to the edges(2 way connections A -> B & B -> A)
+    // directed graph = direction assigned to edge(1 way A -> B is true but B -> A is false)
+    // weighted graph = each connection(edge) has a value
+    // unweighted graph = connections do not have values
+
+
+// matrix: 2 dimensional chart with nested arrays
+
+// adding a vertex
+    // addVertex()
+        // write a method called addVertex, which accepts a name of a vertex
+        // it should add a key to the adjacency list with the name of the vertex and set its value to be an empty array
+
+
+// adding an edge
+    // addEdge()
+        // should accept two vertices, we can call them vertex1 and vertex2
+        // the function should find in the adjacency list the key of vetex1 and push vertex2 to the array
+        // the function should find in the adjacency list the key of vetex2 and push vertex1 to the array
+
+// removing an edge
+    // removeEdge()
+    // this function should accept two vertices, well call them vertex1 and vertex2
+    // the function should reassign the key of vertex1 to be an array that does not contain vertex2
+    // the function should reassign the key of vertex2 to be an array that does not contain vertex1
+
+// removing a vertex
+    // removeVertex()
+        // the function should accept a vertex to remove
+        // the function should loop as long as there are any other vertices in the adjacency list for that vertex
+        // inside of the loop call our removeEdge() with the vertex we are removing and any values in the adjacency list for that vertex
+        // delete the key in the adjacency list for that vertex
+
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+    addVertex(vertex) {
+        // if there isnt any dulicates
+        if(!this.adjacencyList[vertex]){
+            // create an empty array and that vertex
+            this.adjacencyList[vertex] = [];
+        }
+    }
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2); // find in this adjacency list the key of vertex1 and push vertex2 to the array
+        this.adjacencyList[vertex2].push(vertex1); // find in this adjacency list the key of vertex2 and push vertex1 to the array
+    }
+    removeEdge(vertex1, vertex2) {
+        // take current adjacency list of vertex1 and filter it(keep everything that is not equal to vertex2)
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    }
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacencyVertex = this.adjacencyList[vertex].pop()
+            this.removeEdge(vertex, adjacencyVertex);
+            }
+            delete this.adjacencyList[vertex];
+        }
+}
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addEdge("Dallas", "Tokyo")
+g.addEdge("Dallas", "Aspen")
