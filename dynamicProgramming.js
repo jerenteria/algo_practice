8bde1d76492c5919fa0e856a415be21919dc705a
jerenteria <renteria.juan96@gmail.com>
2022-09-25 20:32:27 -0700

    dynamic programming

---
+++ dynamicProgramming.js
@@ -0,0 +1,7 @@
+// what is dynamic programming?
+    //  "A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those
+    // subproblems just once, and storing their solutions"
+    // an approach for solving some problems 
+
+// overlapping subproblems:
+    // small problems that can be reused
