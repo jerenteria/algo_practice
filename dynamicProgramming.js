795124d513250d3a6a1f93cb03c9e8766ef9a03b
jerenteria <renteria.juan96@gmail.com>
2022-09-27 18:32:13 -0700

    back to recurssion

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -5,3 +5,19 @@
 
 // overlapping subproblems:
     // small problems that can be reused
+
+
+// optimal substructure:
+    // a problem is said to have optimal substructure if an optimal solution can be
+    // constructred from optimal solutions of its subproblems
+
+    // ex shortest path from one point to another you can find shortest path from other points
+
+    // simple longest path: simple means no repeats
+
+// fibonacci sequence using recurssion
+
+    function fib(n) {
+        if(n <= 2) return 1; // base case
+        return fib(n-1) + fib(n-2);
+    }
