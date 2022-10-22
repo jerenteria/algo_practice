d99f47eb0ae1e38ed102f7fb7bef98bb88ff734f
jerenteria <renteria.juan96@gmail.com>
2022-10-22 16:07:59 -0700

    easy algo for today

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -913,14 +913,13 @@ var balancedStringSplit = function(s) {
     return count;
 };
 
-// You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
-// Return the number of '*' in s, excluding the '*' between each pair of '|'.
-// Note that each '|' will belong to exactly one pair.
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+        // Input: s = ["h","e","l","l","o"]
+        // Output: ["o","l","l","e","h"]
 
 
-            Input: s = "l|*e*et|c**o|*de|"
-            Output: 2
-            // Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
-            // The characters between the first and second '|' are excluded from the answer.
-            // Also, the characters between the third and fourth '|' are excluded from the answer.
-            // There are 2 asterisks considered. Therefore, we return 2.
+var reverseString = function(s) {  
+    s.reverse(-1);
+};
