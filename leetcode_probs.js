851ed74bca83934596d414ea169e8c3b64a98ff2
jerenteria <renteria.juan96@gmail.com>
2022-10-25 19:30:52 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -963,6 +963,17 @@ var firstPalindrome = function (words) {
     return false
 };
 
+// 412. Fizz Buzz
+
+// Given an integer n, return a string array answer (1-indexed) where:
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+
+
+        // Input: n = 5
+        // Output: ["1","2","Fizz","4","Buzz"]
 
 var fizzBuzz = function(n) {
     return new Array(n).fill(0).map((v, i) => {
