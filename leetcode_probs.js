7945fb57bdd1bf315f72810b6cf252c91652e945
jerenteria <renteria.juan96@gmail.com>
2022-10-26 21:43:01 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -983,3 +983,32 @@ var fizzBuzz = function(n) {
         return `${i+1}`
     })
 };
+
+// 1816. Truncate Sentence
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+
+        // Input: s = "Hello how are you Contestant", k = 4
+        // Output: "Hello how are you"
+        // Explanation:
+        // The words in s are ["Hello", "how" "are", "you", "Contestant"].
+        // The first 4 words are ["Hello", "how", "are", "you"].
+        // Hence, you should return "Hello how are you".
+
+var truncateSentence = function(s, k) {
+    const arr = s.split(" ", k);
+    
+    return arr.join(" ");
+};
+
+// 709. To Lower Case
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+        // Input: s = "Hello"
+        // Output: "hello"
+
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
