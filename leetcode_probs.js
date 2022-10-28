bdc0aae74a0706f836ce6c3925dc383c89313582
jerenteria <renteria.juan96@gmail.com>
2022-10-27 17:33:54 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1012,3 +1012,25 @@ var truncateSentence = function(s, k) {
 var toLowerCase = function(s) {
     return s.toLowerCase();
 };
+
+
+// 1512. Number of Good Pairs
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+//         Input: nums = [1,2,3,1,1,3]
+//         Output: 4
+//         Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+
+var numIdenticalPairs = function(nums) {
+    pairs = 0;
+    
+    for(i = 0; i < nums.length; i++) {
+
+    }
+    return pairs;
+};
