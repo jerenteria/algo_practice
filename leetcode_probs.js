83150dbe46e0fe6d785b923d1d6f5996501b34b9
jerenteria <renteria.juan96@gmail.com>
2022-11-04 19:44:32 -0700

    algos on a friday night after work

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1083,15 +1083,40 @@ var maximumWealth = function(accounts) {
         // - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids
 
 var kidsWithCandies = function(candies, extraCandies) {
+    // goes through all values in candies arr and get the max
     const max = Math.max(...candies);
     let result = [];
 
     for(let i = 0; i < candies.length; i++) {
+        // set currentVal to be current value in arr
         let currentVal = candies[i];
-
+        // push the current value into result and add the extra candies and check if it is greater than or equal to max
         result.push(currentVal + extraCandies >= max)
     }
-
+    
     return result;
 };
 kidsWithCandies([2,3,5,1,3], 3);
+
+// 1512. Number of Good Pairs
+// Given an array of integers nums, return the number of good pairs.
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+        // Example 1:
+
+        // Input: nums = [1,2,3,1,1,3]
+        // Output: 4
+        // Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+var numIdenticalPairs = function(nums) {
+    pairs = 0;
+    
+    for(i = 0; i < nums.length; i++) {
+        for(j = i + 1; j < nums.length; j++) {
+            if(nums[i] === nums[j]) {
+                pairs++;
+            }
+        }
+    }
+    return pairs;
+};
