c9f083052c9d650b94943fc7c7818d8b52accf5f
jerenteria <renteria.juan96@gmail.com>
2022-11-05 21:49:34 -0700

    quick leetcode question before gaming sesh

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1120,3 +1120,31 @@ var numIdenticalPairs = function(nums) {
     }
     return pairs;
 };
+
+// 2006. Count Number of Pairs With Absolute Difference K
+// Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+// The value of |x| is defined as:
+// x if x >= 0.
+// -x if x < 0.
+
+        // Input: nums = [1,2,2,1], k = 1
+        // Output: 4
+        // Explanation: The pairs with an absolute difference of 1 are:
+        // - [1,2,2,1]
+        // - [1,2,2,1]
+        // - [1,2,2,1]
+        // - [1,2,2,1]
+
+var countKDifference = function(nums, k) {
+    var pairs = 0; 
+     
+     for(let i = 0; i < nums.length; i++) {
+         for(let j = i + 1; j < nums.length; j++) {
+             let absoluteValue = Math.abs(nums[i] - nums[j]);
+             if(absoluteValue === k) {
+                 pairs++;   
+             }
+         }
+     }
+     return pairs;
+ };
