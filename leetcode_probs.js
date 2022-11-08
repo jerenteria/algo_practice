ee7318e0de471eb237b1f215556f095a0a55c100
jerenteria <renteria.juan96@gmail.com>
2022-11-07 18:55:54 -0800

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1155,3 +1155,31 @@ var countKDifference = function(nums, k) {
 // For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
 // Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
 // Return the maximum such product difference
+
+
+// 832. Flipping an Image
+// Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+// To flip an image horizontally means that each row of the image is reversed.
+// For example, flipping [1,1,0] horizontally results in [0,1,1].
+// To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+// For example, inverting [0,1,1] results in [1,0,0].
+
+        // Example 1:
+        // Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+        // Output: [[1,0,0],[0,1,0],[1,1,1]]
+        // Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+        // Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+var flipAndInvertImage = function(image) {
+    for(let i = 0 ; i < image.length;i++){
+        image[i].reverse();
+        for(let j = 0;j<image[i].length;j++){
+            if(image[i][j] === 0){
+                image[i][j] = 1;
+            }else{
+              image[i][j] = 0;  
+            }
+        }
+    }
+    return image;
+};
