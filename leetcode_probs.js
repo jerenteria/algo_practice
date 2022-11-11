65c6df2fab14b728dfd785e0e4550c6acd5dbf74
jerenteria <renteria.juan96@gmail.com>
2022-11-10 19:27:36 -0800

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1224,6 +1224,16 @@ var reverseWords = function(s) {
     return reverseWord.join(" ");
 };
 
+// 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+        // Example 1:
+
+        // Input: nums = [1,3,5,6], target = 5
+        // Output: 2
+
 var searchInsert = function(nums, target) {
     for(let i = 0; i < nums.length; i++) {
         if(nums[i] >= target) {
