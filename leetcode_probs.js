b5e0c691bb412fe1cba17b5d447e3b2a93058822
jerenteria <renteria.juan96@gmail.com>
2022-11-12 22:28:57 -0800

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1283,3 +1283,33 @@ var solution = function(isBadVersion) {
 var pivotIndex = function(nums) {
     
 };
+
+// 977. Squares of a Sorted Array
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+        // Example 1:
+        // Input: nums = [-4,-1,0,3,10]
+        // Output: [0,1,9,16,100]
+        // Explanation: After squaring, the array becomes [16,1,0,9,100].
+        // After sorting, it becomes [0,1,9,16,100].
+
+var sortedSquares = function(nums) {
+    let newArr = [];
+    let newSortedArr = [];
+    for(i = 0; i < nums.length; i++) {
+        let newVal = Math.pow(nums[i], 2);
+        newArr.push(newVal);
+        console.log(newArr);
+    }
+    for(let j = 0; j < newArr.length; j++) {
+        for(let k = j + 1; k < newArr.length; k++) {
+            if(nums[j] > nums[k]) {
+                let temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                newSortedArr.push(temp);
+            }
+        }
+    }
+    return newSortedArr;
+};
