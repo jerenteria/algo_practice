07cef9bb21d750005694984ee855123c4cc4a090
jerenteria <renteria.juan96@gmail.com>
2022-11-15 19:24:38 -0800

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1325,3 +1325,35 @@ var sortedSquares = function(nums) {
     return result;
 };
 
+// 189. Rotate Array
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+        // Example 1:
+
+        // Input: nums = [1,2,3,4,5,6,7], k = 3
+        // Output: [5,6,7,1,2,3,4]
+        // Explanation:
+        // rotate 1 steps to the right: [7,1,2,3,4,5,6]
+        // rotate 2 steps to the right: [6,7,1,2,3,4,5]
+        // rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+
+var rotate = function(nums, k) {
+
+  // reverse helper function
+  function reverse(arr, start, end) {
+    while (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
+  }
+
+  k %= nums.length;
+
+  reverse(nums, 0, (nums.length - 1));
+  reverse(nums, 0, (k - 1));
+  reverse(nums, k, (nums.length - 1));
+
+  return nums;
+};
