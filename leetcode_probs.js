9b356111186e839eadbf4b61a53f0be4e36a6fad
jerenteria <renteria.juan96@gmail.com>
2022-11-22 21:05:46 -0800

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -912,7 +912,7 @@ var balancedStringSplit = function(s) {
     }
     return count;
 };
-
+// 344. Reverse String
 // Write a function that reverses a string. The input string is given as an array of characters s.
 // You must do this by modifying the input array in-place with O(1) extra memory.
 
@@ -924,7 +924,20 @@ var balancedStringSplit = function(s) {
 var reverseString = function(s) {  
     s.reverse(-1);
 };
-
+ // BETTER ANSWER- EXPLAINS MORE
+var reverseString2 = function(s) {
+    // Loop through 1/2 of s
+    for (let i = 0; i < s.length / 2; i++) {
+        // Save current val
+        let temp = s[i];
+        
+        // Replace with end of array char
+        s[i] = s[s.length - 1 - i];
+        
+        // Replace end of array letter with current val
+        s[s.length - 1 - i] = temp;
+    }
+};
 
 // 2108. Find First Palindromic String in the Array
 // Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
