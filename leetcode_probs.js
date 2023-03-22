365fd9261dcf6662b8347b0666ad4a68d0c6c5e1
jerenteria <renteria.juan96@gmail.com>
2023-03-21 19:27:25 -0700

    algos

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1520,16 +1520,84 @@ var search = function(nums, target) {
 
 
 // 15. 3Sum
+// Given an integer array nums, return all the triplets 
+// [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
 
-var threeSum = function(nums) {
-    let map = [];
-    for(let i = 0; i < nums.length; i++) {
-        let j = nums[i + 1];
-        let k = nums[j + 2];
+    // Example 1:
+    // Input: nums = [-1,0,1,2,-1,-4]
+    // Output: [[-1,-1,2],[-1,0,1]]
+    // Explanation: 
+    // nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+    // nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+    // nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+    // The distinct triplets are [-1,0,1] and [-1,-1,2].
+    // Notice that the order of the output and the order of the triplets does not matter.
 
-        if(nums[i] + nums[j] + nums[k] == 0) {
-            map.push(nums[i], nums[j], nums[k])
+    // Example 2:
+    // Input: nums = [0,1,1]
+    // Output: []
+    // Explanation: The only possible triplet does not sum up to 0.
+
+
+const twoSum = (j, target, nums) => {
+    let k = nums.length - 1;
+    let result = [];
+
+    while(j < k) {
+        let leftVal = nums[j];
+        let rightVal = nums[k];
+
+        if(leftVal + rightVal > target) {
+            k--;
+        } else if(leftVal + rightVal < target) {
+            j++;
+        } else {
+            result.push([(target * -1), leftVal, rightVal]);
+            while(j < k && nums[j] === nums[j + 1]) j++;
+            while(j < k && nums[k] === nums[k - 1]) k--;
+            j++;
+            k--;
         }
     }
-    return map;
+    return result;
+}
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === nums[i - 1]) continue;
+
+        let target = nums[i] * -1;
+        let subResult = twoSum(i + 1, target, nums);
+        result.push(...subResult);
+    }
+    return result;
+};
+
+// 53. Maximum Subarray
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+//     Example 1:
+//     Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//     Output: 6
+//     Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+//     Example 2:
+//     Input: nums = [1]
+//     Output: 1
+//     Explanation: The subarray [1] has the largest sum 1.
+
+
+var maxSubArray = function(nums) {
+    let solution = nums[0];
+
+    // start at 1 bc we cant check whats behind us
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1])
+        solution = Math.max(solution, nums[i]);
+    }
+    return solution;
 };
