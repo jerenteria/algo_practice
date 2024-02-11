4f8b4c3495e4452f22dc0a6e1f523208a3f34306
jerenteria <renteria.juan96@gmail.com>
2024-02-11 13:20:13 -0800

    solved leetcode question before the super bowl

--- leetcode_probs.js
+++ leetcode_probs.js
@@ -1704,6 +1704,53 @@ var hammingWeight = function(int) {
     return count;
 };
 
+// 26. Remove Duplicates from Sorted Array
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+
+// Example 1:
+
+//     Input: nums = [1,1,2]
+//     Output: 2, nums = [1,2,_]
+//     Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+//     It does not matter what you leave beyond the returned k (hence they are underscores).
+//     Example 2:
+
+//     Input: nums = [0,0,1,1,1,2,2,3,3,4]
+//     Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+//     Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+//     It does not matter what you leave beyond the returned k (hence they are underscores).
+
+var removeDuplicates = function(nums) {
+    // get first pointer to start at index 0
+    let left = 0;
+    // have second pointer start at index 1 and loop through array
+    for(i = 1; i < nums.length; i++) {
+        // loops until it the values at the both pointers are not equal to each other stops to check at (if statement)
+        // if value on the left does not equal the value at index i
+            // l  i
+            // [0,0,1,1,1,2,2,3,3,4]
+        if(nums[left] !== nums[i]) {
+            // then move the left pointer up 1
+            //    l i                       l i                     l     i                 l         i               l           i             l         i                      
+            // [0,0,1,1,1,2,2,3,3,4] = [0,1,1,1,1,2,2,3,3,4] = [0,1,1,1,1,2,2,3,3,4] = [0,1,2,1,1,1,2,3,3,4] = [0,1,2,3,1,2,2,3,3,4] = [0,1,2,3,4,2,2,3,3,4]
+            left++;
+            // and set the value on the left to be equal to the value and index i
+            nums[left] = nums[i]; // changes the value at index left to the value at index i
+            //    l i                       l i                     l     i                 l         i               l           i             l         i                      
+            // [0,0,1,1,1,2,2,3,3,4] = [0,1,1,1,1,2,2,3,3,4] = [0,1,1,1,1,2,2,3,3,4] = [0,1,2,1,1,1,2,3,3,4] = [0,1,2,3,1,2,2,3,3,4] = [0,1,2,3,4,2,2,3,3,4]
+        }
+    }
+    // return the index that left stopped at and add 1 because that is the amount of total non duplicates in array
+    // [0,1,2,3,4,2,2,3,3,4] here the first 4 indexes are non duplicates but the question asks you to place the non duplicates
+    // at the beginning of the array and return how many total non duplicates there are. left has traveled to index 4 but 
+    // there are 5 total non duplicates so you add 1 bc no matter the arr it is always going to start at index 0 so you add 1
+    // to get the total amount of values that we want
+    return left + 1;
+};
+
 // 141. Linked List Cycle
 // Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
