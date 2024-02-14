0652e0f8cca4c9561f32db9b412b97cea3b08f62
jerenteria <renteria.juan96@gmail.com>
2024-02-13 22:14:57 -0800

    algos

--- test.js
+++ test.js
@@ -1,33 +1,47 @@
 // grabbed from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split#examples
-const str = 'The quick brown fox jumps over the lazy dog.';
+const str = "The quick brown fox jumps over the lazy dog.";
 
 // if you put a space in between the '' in split(' ') then it will return the whole word itself unlike the next one const chars
-const words = str.split(' '); // split() grabs the target and turns it into substring by itself and returns the substring
-console.log(words[3]) 
+const words = str.split(" "); // split() grabs the target and turns it into substring by itself and returns the substring
+console.log(words[3]);
 
 // if you dont put a space in split('') then it will return the letter itself unlike above
-const chars = str.split('');
+const chars = str.split("");
 console.log(chars[8]);
-
 
 // Multi Dimensional Selection
 
 function getMaxProduct(arr) {
-    // Write your code here
-    let maxMin = 0;
-    
-    for(let i = 0; i < arr.length; i++) {
-        let currentMin = 0;
-        for(let j = i + 1; j < arr.length; j++) {
-            currentMin -= arr[i][j];
-            console.log(currentMin);
-            console.log(arr[i]);
-            console.log(arr[j] + "---");
-        }
+  // Write your code here
+  let maxMin = 0;
 
-        if(currentMin > maxMin) {
-            currentMin = maxMin;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    let currentMin = 0;
+    for (let j = i + 1; j < arr.length; j++) {
+      currentMin -= arr[i][j];
+      console.log(currentMin);
+      console.log(arr[i]);
+      console.log(arr[j] + "---");
     }
-    return maxMin;
+
+    if (currentMin > maxMin) {
+      currentMin = maxMin;
+    }
+  }
+  return maxMin;
 }
+console.log("----------------------");
+
+var removeElement = function (nums, val) {
+  for (i = 0; i < nums.length; i++) {
+      for(j = nums.length; j <= nums.length - 1; j++) {
+          console.log(nums[j]);
+          if(nums[i] == val) {
+              nums[i] = nums[j];
+          }
+      }
+  }
+  return i;
+    
+};
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
